492e5d9b4678fd25f73fec8e7a667d47b233097e
bdf2ch <bdf2ch@gmail.com>
2020-03-23 23:50:44 +0300

    OSR2: UI fixes

--- projects/operative-situation/src/app/features/operative-situation/components/report/report.component.ts
+++ projects/operative-situation/src/app/features/operative-situation/components/report/report.component.ts
@@ -5,10 +5,11 @@ import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
 import { IApplicationState, } from '../../../../ngrx';
-import {Report, WeatherSummary} from '../../../../models';
+import { Report, WeatherSummary } from '../../../../models';
 import { ReportAddDialogComponent } from '../report-add-dialog/report-add-dialog.component';
-import {selectSelectedDivision, selectSelectedReport, selectSelectedReportWeatherSummary} from '../../ngrx/selectors';
+import { selectSelectedDivision, selectSelectedReport, selectSelectedReportWeatherSummary } from '../../ngrx/selectors';
 import {IDivision} from '../../../../interfaces';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-report',
@@ -19,16 +20,24 @@ export class ReportComponent implements OnInit {
   public selectedDivision$: Observable<IDivision>;
   public selectedReport$: Observable<Report>;
   public selectedReportWeatherSummary: Observable<WeatherSummary>;
+  public inEditConsumptionMode: boolean;
+  public consumptionForm: FormGroup;
 
   constructor(
     private readonly dialog: MatDialog,
+    private readonly builder: FormBuilder,
     private readonly store: Store<IApplicationState>
-  ) {}
+  ) {
+    this.inEditConsumptionMode = false;
+  }
 
   ngOnInit() {
     this.selectedDivision$ = this.store.pipe(select(selectSelectedDivision));
     this.selectedReport$ = this.store.pipe(select(selectSelectedReport));
     this.selectedReportWeatherSummary = this.store.pipe(select(selectSelectedReportWeatherSummary));
+    this.consumptionForm = this.builder.group({
+      consumption: new FormControl(0, Validators.required)
+    });
   }
 
   /**
